262007a0a1c06181d58abc165ff2023d7f8bb686
VladimirX <khrushchev@corpres.ru>
2024-08-03 23:29:08 +0500

    add MapBlock in footer mobile

--- src/components/Footer/ui/MapBlock.jsx
+++ src/components/Footer/ui/MapBlock.jsx
@@ -4,36 +4,36 @@ import {EnvelopeIcon, GpsIcon, PhoneIcon} from "../../../assets/icons";
 const MapBlock = () => {
   return (
     <div className="bg-white">
-      <div className="w-10/12 mx-auto py-24">
+      <div className="w-11/12 sm:w-10/12 mx-auto py-16 sm:py-24">
         <div className="flex flex-col">
-          <p className="text-5xl font-bold">Свяжитесь с нами</p>
-          <p className="mt-5">Мы поможем вам с потребностями в системной интеграции.</p>
+          <p className="text-4xl lg:text-5xl font-bold">Свяжитесь с нами</p>
+          <p className="mt-4 lg:mt-6">Мы поможем вам с потребностями в системной интеграции.</p>
         </div>
-        <div className="flex mt-20">
-          <div className="flex flex-col w-[400px]">
+        <div className="flex flex-col lg:flex-row mt-8 lg:mt-20">
+          <div className="flex flex-col w-full lg:w-[400px]">
             <div className="">
               <EnvelopeIcon/>
-              <p className="font-bold mt-5">Электронная почта</p>
-              <p className="text-base mt-3">Связаться по E-mail</p>
-              <p className="text-base mt-3"><a href="hello@hello.ru">hello@hello.ru</a></p>
+              <p className="font-bold mt-4">Электронная почта</p>
+              <p className="text-base mt-4">Связаться по E-mail</p>
+              <p className="text-base mt-4"><a href="hello@hello.ru">hello@hello.ru</a></p>
             </div>
-            <div className="mt-6">
+            <div className="mt-8">
               <PhoneIcon/>
-              <p className="font-bold mt-5">Телефон</p>
-              <p className="text-base mt-3">Позвоните нам</p>
-              <p className="text-base mt-3"><a href="tel:+78005502533">
-                <nobr>8 800 550-25-33</nobr>
-              </a></p>
+              <p className="font-bold mt-4">Телефон</p>
+              <p className="text-base mt-4">Позвоните нам</p>
+              <p className="text-base mt-4">
+                <a className="underline underline-offset-2" href="tel:+78005502533"><nobr>8 800 550-25-33</nobr></a>
+              </p>
             </div>
-            <div className="mt-6">
+            <div className="mt-8">
               <GpsIcon/>
-              <p className="font-bold mt-5">Офис</p>
-              <p className="text-base mt-3">190000 Санкт-Петербург ул Примерная д 17</p>
+              <p className="font-bold mt-4">Офис</p>
+              <p className="text-base mt-4">190000 Санкт-Петербург ул Примерная д 17</p>
             </div>
           </div>
-          <div className="flex w-[calc(100%-400px)]">
+          <div className="mt-8 lg:mt-0 flex w-full lg:w-[calc(100%-400px)] h-[335px] lg:h-[500px]">
             <iframe className="rounded-2xl" src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=150708584688"
-                    width="100%" height="500"
+                    width="100%" height="100%"
                     frameBorder="0">
 
             </iframe>
