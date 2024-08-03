0816be7bf64b539e39a19a311e6c2603e31c6284
VladimirX <khrushchev@corpres.ru>
2024-08-03 14:48:11 +0500

    ready AdvantagesBlock to mobile

--- src/pages/Main/ui/AdvantagesBlock.jsx
+++ src/pages/Main/ui/AdvantagesBlock.jsx
@@ -3,22 +3,22 @@ import {MedalIcon, StarIcon} from "../../../assets/icons";
 
 const AdvantagesBlock = () => {
   return (
-    <div className="bg-white w-10/12 mx-auto py-24 flex justify-between">
-      <div className="flex flex-col w-1/2">
-        <p className="text-5xl font-bold">Наши преимущества</p>
-        <p className="text-xl font-normal mt-14 leading-8 max-w-[540px]">В своей работе мы используем
+    <div className="bg-white w-11/12 sm:w-10/12 mx-auto py-16 sm:py-24 flex flex-col sm:flex-row justify-between">
+      <div className="flex flex-col w-full sm:w-1/2">
+        <p className="text-4xl sm:text-5xl font-bold">Наши преимущества</p>
+        <p className="text-lg sm:text-xl font-normal mt-8 sm:mt-14 leading-7 sm:leading-8 max-w-[540px]">В своей работе мы используем
           комплексный подход, сочетая IT-решения и бизнес-задачи заказчика.
           Это позволяет нам создавать проекты, которые отвечают современным требованиям и обеспечивают эффективное
           решение задач наших клиентов.</p>
       </div>
-      <div className="flex flex-col w-1/2">
-        <div className="flex justify-end">
-          <div className="flex flex-col w-1/2">
+      <div className="flex flex-col w-full sm:w-1/2 mt-6 sm:my-auto items-center">
+        <div className="flex flex-col sm:flex-row w-full justify-end">
+          <div className="flex flex-col w-full sm:w-1/2 ">
             <MedalIcon/>
             <p className="mt-6 text-xl leading-7">Надежность и качество услуг</p>
             <p className="mt-2 w-52 text-base font-normal opacity-50 leading-6">Более 20 лет успешно работаем на рынке</p>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full sm:w-56 mt-6 sm:mt-0">
             <StarIcon/>
             <p className="mt-6 text-xl leading-7">Персональный подход</p>
             <p className="mt-2 w-52 text-base font-normal opacity-50 leading-6">Индивидуальный подход к каждому клиенту</p>
