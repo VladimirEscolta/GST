f99962111d47a9819a94262ce3f5d7cf56e6c2ad
VladimirX <khrushchev@corpres.ru>
2024-08-04 20:36:45 +0500

    add MainSectionBlock mobile

--- src/components/MainSectionBlock.jsx
+++ src/components/MainSectionBlock.jsx
@@ -14,25 +14,24 @@ const MainSectionBlock = ({data, theme = 'white'}) => {
   }
 
   return (
-    <div className="flex flex-col w-10/12 mx-auto py-24">
-      <div className="flex items-center text-sm text-gst-main">
+    <div className="flex flex-col w-11/12 sm:w-10/12 mx-auto pt-4 pb-16 sm:py-24">
+      <div className="flex flex-wrap items-center text-sm text-gst-main">
         <a href="/" className="flex items-center mr-2">Главная</a>
-
         <p className="text-xl mr-2">›</p>
         <a href="/aksioma" className="flex mr-2">Продукты Аксиома</a>
-
         <p className="text-xl mr-2 opacity-50 text-gst-text">›</p>
-        <p className="flex items-center opacity-50 text-gst-text">{data.name}</p>
-
+        <p className="flex items-center opacity-50 text-gst-text w-full sm:w-auto">{data.name}</p>
       </div>
-      <div className="mt-16 flex items-center justify-between">
-        <div className="w-1/2">
-          <p className="flex p-2.5 w-fit font-bold bg-[#0F86D91A]">{data.header}</p>
-          <p className="mt-6 font-bold text-gst64 leading-gst70">{data.name}</p>
-          <p className="mt-6 text-xl font-medium leading-8">{data.text}</p>
-          <Button className="mt-14" color={'main'} children={'Обсудить проект'} href={'/'}/>
+      <div className="mt-8 sm:mt-16 flex items-center justify-between">
+        <div className="w-full sm:w-1/2">
+          <p className="flex p-2.5 w-fit text-gst15 sm:text-lg leading-gst22 sm:leading-5 font-medium sm:font-bold bg-[#0F86D91A]">{data.header}</p>
+          <p className="mt-6 font-bold text-gst40 sm:text-gst64 leading-gst48 sm:leading-gst70">{data.name}</p>
+          <p className="mt-6 sm:text-xl sm:font-medium leading-6 sm:leading-8">{data.text}</p>
+          <Button className="mt-4 sm:mt-14" color={'main'} children={'Обсудить проект'} href={'/'}/>
         </div>
-        {data.icon}
+        <div className="hidden lg:flex">
+          {data.icon}
+        </div>
       </div>
     </div>
   );
